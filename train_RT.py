aff8b14ba2839d4e4360ae9a6a0215f0edf00a30
Your Name <you@example.com>
2026-01-01 15:40:49 +0800

    新版本的测试通过

--- train_RT.py
+++ train_RT.py
@@ -3,13 +3,13 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
  
 if __name__ == '__main__':
-    #model = RTDETR(model='ultralytics/cfg/models/rt-detr/rtdetr-l.yaml')#定义模型路径
-    model = RTDETR(model='config/rtdetr-l.yaml')
-    # model.load('XXXX.pt')
-    model.train(data='config/data.yaml', epochs=2, batch=4, device='0', imgsz=640, workers=2, deterministic=False,
-                cache=False,amp=True, mosaic=False, project='runs/train', name='test_exp')#定义训练参数
-    # val_status=input("是否进行模型验证：")
-    # if val_status:
-    #     model = RTDETR(model='./runs/train/exp/weights/best.pt')#训练后的模型路径
-    #     model.val(data='config/data.yaml', split='val', batch=1, device='0', project='runs/val', name='exp',
-    #             half=False,)
+    # #model = RTDETR(model='ultralytics/cfg/models/rt-detr/rtdetr-l.yaml')#定义模型路径
+    # model = RTDETR(model='config/rtdetr-l.yaml')
+    # # model.load('XXXX.pt')
+    # model.train(data='config/data.yaml', epochs=2, batch=4, device='0', imgsz=640, workers=2, deterministic=False,
+    #             cache=False,amp=True, mosaic=False, project='runs/train', name='test_exp')#定义训练参数
+    val_status=input("是否进行模型验证：")
+    if val_status:
+        model = RTDETR(model='./runs/train/test_exp2/weights/best.pt')#训练后的模型路径
+        model.val(data='config/data.yaml', split='val', batch=1, device='0', project='runs/val', name='exp',
+                half=False,)
