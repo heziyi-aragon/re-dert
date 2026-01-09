f3ede029b7b28ed5462a45d7fbdab8c0be5f83b5
Your Name <you@example.com>
2026-01-09 11:54:53 +0800

    增加强化训练功能

--- Enforce_Training.py
+++ Enforce_Training.py
@@ -3,12 +3,31 @@
 '''
 from ultralytics import RTDETR
 class Enforce_Training:
-    def __init__(self,model="runs/train/test_exp/weights/best.pt"):
-        self.model=model
+    def __init__(self,model_path="runs/train/test_exp/weights/best.pt"):
+        print(f"正在加载模型：{model_path}")
+        self.model = RTDETR(model_path)
     
     def train(self):
-        self.model.train(data='config/data.yaml',
+        self.model.train(data="config/data.yaml",
+                        epochs=50,
                         imgsz=1024,  # 原先是640，提高分辨率到1024
-                        batch=4,     # 分辨率大了，显存占用会变大，需要减小 batch
+                        batch=12,     # 分辨率大了，显存占用会变大，需要减小 batch
                         lr0=0.0001,  # 降低学习率进行微调
-                        device='0')  
+                        device='0',
+                        project="runs/train_finetune",
+                        name='enforce_exp')  
+    def fina_train(self):
+        print("最后突击")
+        self.model.train(
+            data="config/data.yaml",
+            epochs=30,       
+            imgsz=1024,
+            batch=12,
+            optimizer='AdamW',#显式指定优化器，防止系统自动覆盖 lr0
+            lr0=0.00001,     # 非常小的学习率 (1e-5)
+            lrf=0.01,        # 最终学习率也很小
+            cos_lr=True,     # 开启余弦退火，让学习率平滑下降
+            device='0',
+            project="runs/train_finetune",
+            name='enforce_exp_final_boost'
+        )
--- train_RT.py
+++ train_RT.py
@@ -6,16 +6,36 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 if __name__ == '__main__':
     #model = RTDETR(model='ultralytics/cfg/models/rt-detr/rtdetr-l.yaml')#定义模型路径
     val_status=input("你接下来要进行什么操作？ 1：训练模型。\n2：验证模型。3：升级模型。")
-    if val_status==1:
+    if val_status=="1":
+        print("准备训练模型")
         model = RTDETR(model='config/rtdetr-l.yaml')
         # model.load('XXXX.pt')
         model.train(data='config/data.yaml', epochs=2, batch=4, device='0', imgsz=640, workers=2, deterministic=False,
                     cache=False,amp=True, mosaic=False, project='runs/train', name='test_exp')#定义训练参数
-    elif val_status==2:
-        model = RTDETR(model='./runs/train/test_exp2/weights/best.pt')#训练后的模型路径
-        model.val(data='config/data.yaml', split='val', batch=1, device='0', project='runs/val', name='exp',
-                half=False)
-    elif val_status==3:
-        url="runs/train/test_exp/weights/best.pt"
-        Train=Enforce_Training(url)
-        Train.train()
+    elif val_status=="2":#验证
+        print("准备验证模型")
+        choose=input(f"接下来是验证模型还是继续增强？1：验证模型。2：TTA增强模型。")
+        if choose =="1":
+            model = RTDETR(model='./runs/train/test_exp/weights/best.pt')#训练后的模型路径
+            model.train(data='config/data.yaml', epochs=100, batch=16, device='0', imgsz=640, workers=2, deterministic=False,
+                        cache=False,amp=True, mosaic=False, project='runs/train', name='test_exp')#定义训练参数
+        elif choose =="2":#TTA (测试时增强)
+            model = RTDETR(model="./runs/train_finetune/enforce_exp3/weights/best.pt")
+            metrics = model.val(data='config/data.yaml',split='test',imgsz=1024, batch=1,augment=True)
+            print(f"TTA mAP50: {metrics.box.map50}")
+    elif val_status=="3":#增强训练
+        print("准备增强模型")
+        choose=input("接下来是衔接上一轮继续还是选择最好的继续？1：衔接上一轮继续。2：从最好的开始。3：如果运行2之后还不满意")
+        if choose=="1":
+            url="runs/train/test_exp/weights/last.pt" #衔接继续学：如果根据日志,100轮前损失函数（Loss）还在下降，说明模型还能学
+            model = RTDETR(model='./runs/train/test_exp/weights/last.pt')#训练后的模型路径
+            model.train(resume=True)
+        elif choose=="2":
+            #url="runs/train/test_exp/weights/best.pt"#增强学习
+            url="runs/train_finetune/enforce_exp2/weights/best.pt"#增强学习
+            Train=Enforce_Training(url)
+            Train.train()
+        elif choose=="3":
+            url="runs/train_finetune/enforce_exp3/weights/best.pt"#增强学习
+            Train=Enforce_Training(url)
+            Train.fina_train()
